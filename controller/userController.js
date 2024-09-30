71ca949c481454382bc243af46591f62bfaac10c
Balajiaswat <Oxygenkart@2024>
2024-09-30 11:40:24 +0530

    done

--- controller/userController.js
+++ controller/userController.js
@@ -3,7 +3,7 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const { BlackListModel } = require("../model/blacklist");
 const admin = require("firebase-admin");
-const serviceAccount = require("../oxygenkart-3c9ef-firebase-adminsdk-omjq3-231069c64d.json");
+// const serviceAccount = require("../oxygenkart-3c9ef-firebase-adminsdk-omjq3-231069c64d.json");
 const { getAuth } = require("firebase-admin/auth");
 require("dotenv").config();
 
@@ -64,7 +64,17 @@ const login = async (req, res) => {
 };
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"), // Ensure correct formatting
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_CLIENT_ID,
+    auth_uri: process.env.FIREBASE_AUTH_URI,
+    token_uri: process.env.FIREBASE_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
+    client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
+  }),
 });
 
 const googleAuthLogin = async (req, res) => {
