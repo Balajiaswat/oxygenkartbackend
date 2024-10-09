dd77c620eb8954f834ccecc59fa084bc6ef5dd98
Balajiaswat <Oxygenkart@2024>
2024-10-09 11:14:00 +0530

    done

--- config/DB.js
+++ config/DB.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const connecttion = mongoose.connect(
-  "mongodb+srv://balajiaswat:KIL1aUQmviUYNvjI@oxygenkartcluster.6pztc.mongodb.net/?retryWrites=true&w=majority&appName=OxygenKartCluster"
+  "mongodb+srv://balajiaswat:KIL1aUQmviUYNvjI@oxygenkartcluster.6pztc.mongodb.net/oxygenData?retryWrites=true&w=majority&appName=OxygenKartCluster"
 );
 
 module.exports = connecttion;
